dd1db3ed19c85f2fb7f74e153f899b3dddd80c0e
Opio Daniel <danielopio540@gmail.com>
2025-08-27 14:45:21 +0300

    update vite.config.ts to include tailwind css

--- abujas-frontend/vite.config.ts
+++ abujas-frontend/vite.config.ts
@@ -1,10 +1,7 @@
-// vite.config.ts
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-
+import { defineConfig } from 'vite'
+import tailwindcss from '@tailwindcss/vite'
 export default defineConfig({
-  plugins: [react()],
-  build: {
-    outDir: 'dist',
-  },
-});
+  plugins: [
+    tailwindcss(),
+  ],
+})
